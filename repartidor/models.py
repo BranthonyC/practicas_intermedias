1f2e5ba4e695e70b47eea9018c5b722efd6f05bb
HPecene <34321539+HPecene@users.noreply.github.com>
2020-10-26 23:44:49 -0600

    Modelo Repartidor
    Se migro el modelo a la Aplicacion

--- repartidor/models.py
+++ repartidor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Repartidor(models.Model):
+    dpi = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=40)
+    nacimiento = models.DateField()
+    correo = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.dpi
