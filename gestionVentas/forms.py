ed6f58ef84bef71b57f79b1fdd9215a7c2f01c8b
kevgoz <kelvin_cplay@hotmail.com>
2020-11-02 13:07:29 -0600

    agregando descuento a los productos

--- gestionVentas/forms.py
+++ gestionVentas/forms.py
@@ -15,9 +15,27 @@ TIPO_VENTA= [
   
     ]
 
+
+TIPO_DESCUENTO= [
+    ('CON_DESCUENTO', 'CON DESCUENTO'),
+    ('SIN_DESCUENTO', 'SIN DESCUENTO'),
+  
+    ]
+
+DESCUENTO= [
+    ('SIN_DESCUENTO', 'SIN_DESCUENTO'),
+    ('CINCO', 'APLICAR 5%'),
+    ('DIEZ', 'APLICAR 10%'),
+    ('QUINCE', 'APLICAR 15%'),
+  
+    ]
+DETALLE= [
+    ('A', '-----'),
+    
+    ]
 ### SOLO EL USUARIO VENDEDOR PUEDE CREAR UNA NUEVA VENTAS
 class Crear_ventaForm(forms.Form):  
-    no_orden= forms.IntegerField() 
+    #no_orden= forms.IntegerField() 
     cliente=forms.ModelChoiceField(queryset=Cliente.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
     fecha_facturacion = forms.DateField(initial=datetime.date.today, required=False)
     fecha_entrega = forms.DateField(initial=datetime.date.today, required=False)
@@ -26,12 +44,22 @@ class Crear_ventaForm(forms.Form):
    
 class ListaProductosForm(forms.Form):   
     
-    no_orden=forms.ModelChoiceField(queryset=Venta.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
-    #producto2=forms.ModelChoiceField(queryset=Producto.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    #no_orden=forms.ModelChoiceField(queryset=Venta.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
     producto=forms.ModelChoiceField(queryset=Producto.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
     cantidad= forms.IntegerField()
+    porcentaje = forms.CharField(label='Aplicar descuentooo', widget=forms.Select(choices=DESCUENTO))
     
-  
+class Seleccionar_ordenForm(forms.Form):   
+    no_orden=forms.ModelChoiceField(queryset=Venta.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    desc = forms.CharField(label='Aplicar descuento', widget=forms.Select(choices=DESCUENTO))
+
+class Seleccionar_DetalleForm(forms.Form):   
+    no_orden=forms.ModelChoiceField(queryset=Venta.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    tipo_venta = forms.CharField(label='Tipo de descuento', widget=forms.Select(choices=TIPO_DESCUENTO))
+
+class Seleccionar_orden2Form(forms.Form):   
+    #no_orden=forms.ModelChoiceField(queryset=Venta.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+    desc2 = forms.CharField(label='Ordenes', widget=forms.Select(choices=DETALLE))
 
 class NuevaListaForm(forms.ModelForm):
     class Meta:
--- gestionVentas/views.py
+++ gestionVentas/views.py
@@ -1,30 +1,52 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from producto.models import Producto
-from django.http import HttpResponse
-from .forms import Crear_ventaForm,NuevaVentaForm,NuevaListaForm,ListaProductosForm
+from django.http import HttpResponse,HttpResponseRedirect
+from .forms import Crear_ventaForm,NuevaVentaForm,NuevaListaForm,ListaProductosForm,Seleccionar_ordenForm,Seleccionar_DetalleForm,Seleccionar_orden2Form
 # Create your views here.
 from django.contrib import messages
 from .models import Venta,ListaProductos
 from cliente.models import Cliente
+import random
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+import datetime
 ## SOLO USUARIOS VENDEDOR PUEDEN CREAR VENTAS
+
+## para pdf
+from django.template.loader import render_to_string
+from weasyprint import HTML
+import tempfile
+
+from django.db.models import Sum
+from .admin import ListaAdmin
+
+
 def Crear_venta(request):
     
+    
+    orden_random = random.randrange(100000)    
     orden =0
     if request.method == 'POST':
         form = Crear_ventaForm(request.POST)
         form2 = ListaProductosForm(request.POST)
+        form4 = Seleccionar_orden2Form(request.POST)
+        #form_factura1 = Seleccionar_DetalleForm(request.POST)
         if form.is_valid():
-            orden = form.data['no_orden']
+            
+            #orden = form.data['no_orden']
+            orden = orden_random
+            
             cliente = form.cleaned_data['cliente']
             fecha_fac = form.cleaned_data['fecha_facturacion']
             fecha_entr = form.cleaned_data['fecha_entrega']
             tipo_venta_v = form.cleaned_data['tipo_venta']
-            #cliente = form.cleaned_data['cliente']
-            vendedor_v="Kelvin"
+            
+            vendedor_2 = request.user.username
+            vendedor_v=vendedor_2
             Clte=Cliente.objects.get(nombre=cliente)
             crear_orden=Venta(
-                no_orden= orden,
+                no_orden= orden_random,
                 cliente=Clte,
                 vendedor=vendedor_v,
                 fecha_facturacion = fecha_fac,
@@ -34,31 +56,181 @@ def Crear_venta(request):
                
             )
             crear_orden.save()#Se guarda el historico
+
         if form2.is_valid():
-            no_orden2 = form2.cleaned_data['no_orden']
-            print("orden" + str(no_orden2))
+            #no_orden2 = form2.cleaned_data['no_orden']
+            #print("orden" + str(no_orden2))
+            
             producto = form2.cleaned_data['producto']
             cantidad2 = form2.cleaned_data['cantidad']
+            porcentaje = form2.cleaned_data['porcentaje']
 
             Prod=Producto.objects.get(nombre=producto)
+            no_orden=Venta.objects.latest('id')
+            econtro=ListaProductos.objects.filter(no_orden=no_orden).filter(producto=producto).values()
             
-            nueva_orden=ListaProductos(
-                no_orden= no_orden2,
-                producto=Prod,
-                cantidad=cantidad2,
-                precio = Prod.precio,
-                subtotal = cantidad2 * float(Prod.precio)
+            precio_parcial = cantidad2 * float(Prod.precio)
+            precio_real = precio_parcial
+            descontar = 1
+            if porcentaje == 'SIN_DESCUENTO':
+                precio_real
                 
-               
-            )
-            nueva_orden.save()
-            messages.success(request, 'Producto registrado correctamente')
+            elif porcentaje == 'CINCO':
+                print("sdf")
+                precio_real = precio_parcial - (precio_parcial * 0.05)
+                descontar = 0.05
+            elif porcentaje == 'DIEZ':
+                precio_real = precio_parcial - (precio_parcial * 0.10)
+                print("sdf")
+            elif porcentaje == 'QUINCE':
+                print("sdf")
+                precio_real = precio_parcial - (precio_parcial * 0.15)
+            else:
+                descontar = 1
+
+            if econtro.count() == 0:
+                
+                nueva_orden=ListaProductos(
+                    no_orden= no_orden,
+                    producto=Prod,
+                    cantidad=cantidad2,
+                    precio = Prod.precio,
+                    subtotal = precio_real
+                )
+                nueva_orden.save()
+            else :
+                #sub_temp = cantidad2 * float(Prod.precio)
+                #sub_temp1 = sub_temp - (sub_temp * descontar)
+                modificar_item=ListaProductos.objects.get(no_orden=no_orden,producto=producto)
+                modificar_item.cantidad = cantidad2
+                modificar_item.subtotal = precio_real
+                modificar_item.save()
+                messages.success(request, 'Se agrego')
             return redirect('Crear_venta')  
+    
+        if form4.is_valid():
+            
+            no_=Venta.objects.latest('id')
+            form_factura1 = Seleccionar_DetalleForm(request.POST)
+            #no_ = form4.cleaned_data['no_orden']
+            venta=Venta.objects.get(no_orden=no_)
+            tipo = venta.tipo_venta
+            context = {}
+            
+            lista_ = ListaProductos.objects.filter(no_orden=no_)
+            total = ListaProductos.objects.filter(no_orden=no_).aggregate(Sum('subtotal'))
+            total_ = total['subtotal__sum']
+            total_descuento = total_
+            if tipo == 'DOMICILIO':
+                total_descuento = total_descuento  + (total_descuento * 0.10)
+            else:
+                total_descuento 
+            #return HttpResponseRedirect('/lista_detalle/'+str(no_))
+            return render(request,'ventas/lista_detalle.html',{'lista_productos' : lista_,'total': total_,'tot_desc':total_descuento,'tipo':tipo,'orden':no_,'cliente':venta.cliente})
+            #return redirect('Crear_venta')
+        
     else:
         #HttpResponse("funciona")
         form=Crear_ventaForm()
         form2=ListaProductosForm()
-    return render(request,'ventas/crear_ventas.html',{'form': form,'form2': form2 })
+        form4=Seleccionar_orden2Form()
+    return render(request,'ventas/crear_ventas.html',{'form': form,'form2': form2 ,'form4':form4})
+    
+def Agregar_producto(request,no_orden):
+    
+   return HttpResponse("funciona")
 
-    #return HttpResponse("funciona")
 
+class ListaProductosListView(ListView):
+    model = ListaProductos
+    template_name = "ventas/lista_detalle.html"
+    context_object_name = "lista_productos"
+
+    def get_queryset(self):
+        return ListaProductos.objects.filter(no_orden=self.kwargs['no_orden'])
+   
+
+def export_pdf(request):
+
+    response = HttpResponse(content_type='application/pdf')
+    #response['Content-Disposition'] = 'inline; attachment; filename=Sistema_de_BodegasG8_' + str(datetime.datetime.now())+ '.pdf'
+    response['Content-Disposition'] = 'attachment; filename=Sistema_de_BodegasG8_' + str(datetime.datetime.now())+ '.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+
+    no_o=6892
+    #expenses = ListaProductos.objects.filter(no_orden=no_o)
+    expenses = ListaProductos.objects.filter(no_orden=no_o)
+   
+    sum = ListaProductos.objects.aggregate(Sum('subtotal'))
+    
+    #html_string = render_to_string('ventas/pdf_factura.html',{'expenses':[],'total':0})
+    html_string = render_to_string('ventas/pdf_factura.html',{'expenses':expenses,'total':sum})
+    #html_string = render_to_string('ventas/pdf_factura.html',{'expenses':expenses})
+
+    html = HTML(string = html_string)
+
+    result = html.write_pdf()
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        output = open(output.name,'rb')
+        response.write(output.read())
+
+    return response
+
+
+#def export_pdf2(request,pk):
+def export_pdf2(request,pk):
+    response = HttpResponse(content_type='application/pdf')
+    #response['Content-Disposition'] = 'inline; attachment; filename=Sistema_de_BodegasG8_' + str(datetime.datetime.now())+ '.pdf'
+    response['Content-Disposition'] = 'attachment; filename=Sistema_de_BodegasG8_' + str(datetime.datetime.now())+ '.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+
+    no_o=40242
+    #expenses = ListaProductos.objects.filter(no_orden=no_o)
+    expenses = ListaProductos.objects.filter(no_orden=pk)
+    venta=Venta.objects.get(no_orden=pk)
+    cliente = venta.cliente
+    vendedor = venta.vendedor
+    fecha_fac  = venta.fecha_facturacion
+    fecha_entrega = venta.fecha_entrega
+
+   
+    tipo = venta.tipo_venta
+    sum = ListaProductos.objects.filter(no_orden=pk).aggregate(Sum('subtotal'))
+    sub_total = sum['subtotal__sum']
+    total_recargo = sub_total
+    if tipo == 'DOMICILIO':
+        total_recargo = total_recargo  + (total_recargo * 0.10)
+    else:
+        total_recargo
+
+    context = {}
+    context["cliente"] = cliente
+    context["vendedor"] = vendedor
+    context["fecha_factura"] = fecha_fac
+    context["fecha_entrega"] = fecha_entrega
+    context["expenses"] = expenses
+    context["sub_total"] = sub_total
+    context["total_recargo"] = total_recargo
+    context["tipo"] = tipo
+
+    html_string = render_to_string('ventas/pdf_factura.html',context)
+    #html_string = render_to_string('ventas/pdf_factura.html',{'expenses':expenses})
+    #html_string = render_to_string('ventas/pdf_factura.html',{'expenses':expenses,'total':sub_total,'tipo':tipo,'total_recargo':total_recargo,'context':context})
+    
+
+    html = HTML(string = html_string)
+
+    result = html.write_pdf()
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        output = open(output.name,'rb')
+        response.write(output.read())
+
+    return response
+
+    #return redirect('Crear_venta') 
